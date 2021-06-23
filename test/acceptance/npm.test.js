54607cd68b13bb7b404b63396d9dce5ed3ee3693
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-06-23 15:45:35 -0400

    test: use more precise assertions

--- test/acceptance/npm.test.js
+++ test/acceptance/npm.test.js
@@ -3,102 +3,165 @@ const fs = require('fs');
 const path = require('path');
 const v = '2.0.0';
 
-function getPkg() {
-  return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf8'));
-}
-
 function loadFile(fileName) {
   return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../fixtures/' + fileName), 'utf8'));
 }
 
-it('add(test)', () => {
-  const pkg = getPkg();
+// these are in each fixture package.json
+const fixtureDependencies = {
+  '@google-cloud/promisify': '^0.3.0',
+  '@snyk/email-templates': '1.14.1',
+  debug: '2.6.7',
+  lodash: '^1.3.1',
+  semver: '3.0.0'
+}
 
+it('add(test)', () => {
+  const pkg = {};
   lib.add(pkg, 'test', v);
-  expect(pkg.scripts.test).toContain('snyk test');
-  expect(pkg.devDependencies.snyk).toBe('^' + v);
+
+  expect(pkg).toEqual({
+    scripts: {
+      test: 'snyk test'
+    },
+    devDependencies: {
+      snyk: `^${v}`,
+    }
+  });
 });
 
 it('add(protect)', () => {
-  const pkg = getPkg();
-
+  const pkg = {};
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.prepare).toContain('npm run snyk-protect');
-  expect(pkg.scripts.prepublish).toBeUndefined();
-
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  
+  expect(pkg).toEqual({
+    scripts: {
+      prepare: 'npm run snyk-protect',
+      'snyk-protect': 'snyk protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    snyk: true,
+  });
 });
 
 it('script exists but not snyk protect (protect)', () => {
   const pkg = loadFile('missing-snyk-protect-package.json');
-
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.prepublish).toContain('npm run snyk-protect');
-  expect(pkg.scripts.prepare).toBeUndefined();
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      prepublish: 'npm run snyk-protect',
+      // 'snyk-protect': 'snyk protect',  // this is not in the results - is this a bug?
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies
+    },
+    snyk: true,
+  });
 });
 
 it('do not add another script if one exists (protect)', () => {
   const pkg = loadFile('with-prepublish-package.json');
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.prepublish).toContain('npm run snyk-protect');
-  expect(pkg.scripts.prepare).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepublish': 'npm run snyk-protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies,
+    },
+    snyk: true,
+  });
 });
 
 it('update the same script that exists (protect)', () => {
   const pkg = loadFile('prepublish-without-snyk-package.json');
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.prepublish).toBe('npm run snyk-protect && npm run build');
-  expect(pkg.scripts.prepare).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepublish': 'npm run snyk-protect && npm run build',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies,
+    },
+    snyk: true,
+  });
 });
 
 it('if both prepare/prepublish exists update first one (protect)', () => {
   const pkg = loadFile('with-prepare-and-prepublish-package.json');
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.prepare).toBe('npm run snyk-protect && npm run test');
-  expect(pkg.scripts.prepublish).toBe('npm run build');
-
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepublish': 'npm run build',
+      'prepare': 'npm run snyk-protect && npm run test',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies,
+    },
+    snyk: true,
+  });
 });
 
 it('default to prepare (protect)', () => {
-  const pkg = getPkg();
+  const pkg = {};
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.prepare).toBe('npm run snyk-protect');
-  expect(pkg.scripts.prepublish).toBeUndefined();
-
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  
+  expect(pkg).toEqual({
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepare': 'npm run snyk-protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    snyk: true,
+  });
 });
 
 it('add(protect) npm 5', () => {
-  const pkg = getPkg();
-
+  const pkg = {};
   lib.add(pkg, 'protect', v, 'prepare');
-  expect(pkg.scripts.prepare).toContain('npm run snyk-protect');
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+
+  expect(pkg).toEqual({
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepare': 'npm run snyk-protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    snyk: true,
+  });
 });
 
 it('add(test && protect) on empty package', () => {
   const pkg = {
     name: 'empty',
   };
-
   lib.add(pkg, 'test', v);
   lib.add(pkg, 'protect', v);
-  expect(pkg.scripts.test).toContain('snyk test');
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-
+  
   expect(pkg).toEqual({
     name: 'empty',
     scripts: {
@@ -115,14 +178,27 @@ it('add(test && protect) on empty package', () => {
 });
 
 it('already testing moves to prod deps when protect', () => {
-  const pkg = getPkg();
-  const oldVersion = '1.0.0';
-  pkg.devDependencies.snyk = oldVersion;
-  pkg.scripts.test = ' && snyk test';
+  const pkg = {
+    scripts: {
+      test: ' && snyk test',
+    },
+    devDependencies: {
+      snyk: '1.0.0',
+    },
+  };
 
   lib.add(pkg, 'protect', v, 'prepare');
-  expect(pkg.scripts.prepare).toContain('npm run snyk-protect');
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.devDependencies.snyk).toBeUndefined();
-  expect(pkg.snyk).toBe(true);
+  
+  expect(pkg).toEqual({
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      prepare: 'npm run snyk-protect',
+      test: ' && snyk test',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    devDependencies: {},
+    snyk: true,
+  });
 });
--- test/acceptance/yarn.test.js
+++ test/acceptance/yarn.test.js
@@ -3,91 +3,148 @@ const fs = require('fs');
 const path = require('path');
 const v = '2.0.0';
 
-function getPkg() {
-  return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf8'));
-}
-
 function loadFile(fileName) {
   return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../fixtures/' + fileName), 'utf8'));
 }
 
-it('add(test)', () => {
-  const pkg = getPkg();
+// these are in each fixture package.json
+const fixtureDependencies = {
+  '@google-cloud/promisify': '^0.3.0',
+  '@snyk/email-templates': '1.14.1',
+  debug: '2.6.7',
+  lodash: '^1.3.1',
+  semver: '3.0.0'
+}
 
+it('add(test)', () => {
+  const pkg = {};
   lib.add(pkg, 'test', v, undefined, 'yarn');
-  expect(pkg.scripts.test).toContain('snyk test');
-  expect(pkg.devDependencies.snyk).toBe('^' + v);
+
+  expect(pkg).toEqual({
+    scripts: {
+      test: 'snyk test'
+    },
+    devDependencies: {
+      snyk: `^${v}`,
+    }
+  });
 });
 
 it('add(protect)', () => {
-  const pkg = getPkg();
+  const pkg = {};
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepare).toContain('yarn run snyk-protect');
-  expect(pkg.scripts.prepublish).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    scripts: {
+      prepare: 'yarn run snyk-protect',
+      'snyk-protect': 'snyk protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    snyk: true,
+  });
 });
 
 it('script exists but not snyk protect (protect)', () => {
   const pkg = loadFile('missing-snyk-protect-package-yarn.json');
-
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepublish).toContain('yarn run snyk-protect');
-  expect(pkg.scripts.prepare).toBeUndefined();
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      prepublish: 'yarn run snyk-protect',
+      // 'snyk-protect': 'snyk protect',  // this is not in the results - is this a bug?
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies
+    },
+    snyk: true,
+  });
 });
 
 it('do not add another script if one exists (protect)', () => {
   const pkg = loadFile('with-prepublish-package-yarn.json');
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepublish).toBe('yarn run snyk-protect');
-  expect(pkg.scripts.prepare).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepublish': 'yarn run snyk-protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies,
+    },
+    snyk: true,
+  });
 });
 
 it('update the same script that exists (protect)', () => {
   const pkg = loadFile('prepublish-without-snyk-package-yarn.json');
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepublish).toBe('yarn run snyk-protect && yarn run build');
-  expect(pkg.scripts.prepare).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepublish': 'yarn run snyk-protect && yarn run build',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies,
+    },
+    snyk: true,
+  });
 });
 
 it('if both prepare/prepublish exists update first one (protect)', () => {
   const pkg = loadFile('with-prepare-and-prepublish-package-yarn.json');
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepare).toBe('yarn run snyk-protect && yarn run test');
-  expect(pkg.scripts.prepublish).toBe('yarn run build');
-
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepublish': 'yarn run build',
+      'prepare': 'yarn run snyk-protect && yarn run test',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies,
+    },
+    snyk: true,
+  });
 });
 
 it('default to prepare (protect)', () => {
-  const pkg = getPkg();
+  const pkg = {};
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepare).toBe('yarn run snyk-protect');
-  expect(pkg.scripts.prepublish).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      'prepare': 'yarn run snyk-protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    snyk: true,
+  });
 });
 
 it('add(test && protect) on empty package', () => {
   const pkg = {
     name: 'empty',
   };
-
   lib.add(pkg, 'test', v, undefined, 'yarn');
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.test).toContain('snyk test');
-  expect(pkg.dependencies.snyk).toBe('^' + v);
 
   expect(pkg).toEqual({
     name: 'empty',
@@ -104,36 +161,66 @@ it('add(test && protect) on empty package', () => {
   });
 });
 
-
 it('already testing moves to prod deps when protect', () => {
-  const pkg = getPkg();
-  const oldVersion = '1.0.0';
-  pkg.devDependencies.snyk = oldVersion;
-  pkg.scripts.test = ' && snyk test';
-
+  const pkg = {
+    scripts: {
+      test: ' && snyk test',
+    },
+    devDependencies: {
+      snyk: '1.0.0',
+    },
+  };
+  
   lib.add(pkg, 'protect', v, 'prepare', 'yarn');
-  expect(pkg.scripts.prepare).toContain('yarn run snyk-protect');
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.devDependencies.snyk).toBeUndefined();
-  expect(pkg.snyk).toBe(true);
+
+  expect(pkg).toEqual({
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      prepare: 'yarn run snyk-protect',
+      test: ' && snyk test',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+    },
+    devDependencies: {},
+    snyk: true,
+  });
 });
 
 it('update the same script that exists (protect with extra commands) from npm to yarn', () => {
   const pkg = loadFile('with-prepublish-npm-but-now-yarn.json');
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepublish).toBe('yarn run snyk-protect && yarn run build');
-  expect(pkg.scripts.prepare).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      prepublish: 'yarn run snyk-protect && yarn run build',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies
+    },
+    snyk: true,
+  });
 });
 
 it('update the same script that exists (protect) from npm to yarn', () => {
   const pkg = loadFile('npm-with-prepublish-simple-now-yarn.json');
   lib.add(pkg, 'protect', v, undefined, 'yarn');
-  expect(pkg.scripts.prepublish).toBe('yarn run snyk-protect');
-  expect(pkg.scripts.prepare).toBeUndefined();
 
-  expect(pkg.dependencies.snyk).toBe('^' + v);
-  expect(pkg.snyk).toBe(true);
+  expect(pkg).toEqual({
+    name: 'package-lock-exact-match',
+    version: '1.0.0',
+    scripts: {
+      'snyk-protect': 'snyk protect',
+      prepublish: 'yarn run snyk-protect',
+    },
+    dependencies: {
+      snyk: `^${v}`,
+      ...fixtureDependencies
+    },
+    snyk: true,
+  });
 });
